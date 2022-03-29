02d53eaa8d2e33d0f19e9e3cac62535c4f492c9c
zalik198 <zshomakhov@mail.ru>
2022-03-30 00:44:27 +0300

    start work

--- MyHabits/AppDelegate.swift
+++ MyHabits/AppDelegate.swift
@@ -11,25 +11,40 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let tabBarController = UITabBarController()
+        self.window?.rootViewController = tabBarController
+        
+        let habitsViewController = HabitsViewController()
+        habitsViewController.view.backgroundColor = .lightGray
+        let habitsNavigationController = UINavigationController(rootViewController: habitsViewController)
+        habitsNavigationController.tabBarItem = UITabBarItem(title: "Привычки", image: UIImage(named: "habitsIcon"), selectedImage: nil)
+        
+        let infoViewController = InfoViewController()
+        let infoNavigationController = UINavigationController(rootViewController: infoViewController)
+        infoNavigationController.tabBarItem = UITabBarItem(title: "Информация", image: UIImage(named: "info"), selectedImage: nil)
+        
+        tabBarController.viewControllers = [habitsNavigationController, infoNavigationController]
+        tabBarController.tabBar.isHidden = false
+        tabBarController.tabBar.backgroundColor = .white
+        
+        let appearance = UINavigationBarAppearance()
+        UINavigationBar.appearance().barTintColor = .white
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        
+        
+        self.window?.rootViewController = tabBarController
+        self.window?.makeKeyAndVisible()
         return true
+        
+        
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
 
 
 }
---
+++ MyHabits/InfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InfoViewController.swift
+//  MyHabits
+//
+//  Created by Shom on 29.03.2022.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
